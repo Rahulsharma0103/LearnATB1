4fdf6039217eac6430f4785d3ce212a4e2575a44
Rahulsharma0103 <sharma.rs0103@gmail.com>
2025-12-16 12:46:28 +0530

    first commit 16 dec

--- src/Helloworld.java
+++
@@ -1,4 +0,0 @@
-public class Helloworld {
-
-    system.out.println("Hello");
-}
---
+++ src/ex01_JavaBasics/Lab001_Helloworld.java
@@ -0,0 +1,8 @@
+package ex01_JavaBasics;
+
+public class Lab001_Helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
